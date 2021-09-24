81e7792ad36f37c477cc496d52d810b486590271
Drew Benson <drew@adbenson.net>
2021-09-23 20:06:03 -0400

    Remove a bunch of unused stuff from main Game component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,20 +1,16 @@
 import { useAppDispatch, useAppSelector } from "../app/hooks";
-import { Card } from "../game/card-models";
-import { addPlayers, deal, selectPlayers, selectTable } from "../game/game-slice";
+import { addPlayers, selectPlayers } from "../game/game-slice";
 import { GamePhase } from "../game/game-state";
 import { Table } from "./Table";
 
 export function Game() {
-	const table = useAppSelector(selectTable);
+
 	const players = useAppSelector(selectPlayers);
 	const phase = useAppSelector(state => state.game.phase);
 
-
 	const dispatch = useAppDispatch();
 
-	const handCardClickHandler = (player: number, card: Card): void => {console.log(player, card)};
 	const playerButtonHandler = () => dispatch(addPlayers(["Anne", "Bob", "Charlie", "Diane"]));
-	const dealButtonHandler = () => dispatch(deal());
 
 	if (phase === GamePhase.ADD_PLAYERS) {
 		return (
@@ -23,12 +19,9 @@ export function Game() {
 				{players.map(p => p.name).join(", ")}
 			</div>
 		);
-	} else if (phase === GamePhase.DEAL) {
-		return <button onClick={dealButtonHandler}>Deal</button>
 	} else {
 		return (
 			<div>
-				{phase}
 				<Table></Table>
 			</div>
 		);
