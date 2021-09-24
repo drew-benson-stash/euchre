5c0b12cabe2f1c1387da2805451bda6728b34274
Drew Benson <drew@adbenson.net>
2021-09-24 11:16:42 -0400

    Refining mini cards

--- src/components/MiniCard.tsx
+++ src/components/MiniCard.tsx
@@ -1,15 +1,24 @@
-import { Card, shortValue, suitToFilename } from "../game/card-models";
+import { Card, CardSuit, shortValue, suitToFilename } from "../game/card-models";
 import styles from "./MiniCard.module.css";
 
 export interface MiniCardProps {
 	readonly card: Card;
 }
 
+const suitToColor: Record<CardSuit, string> = {
+	[CardSuit.CLUBS]: "black",
+	[CardSuit.SPADES]: "black",
+	[CardSuit.DIAMONDS]: "red",
+	[CardSuit.HEARTS]: "red",
+}
+
 export function MiniCard(props: MiniCardProps) {
+	const suit = props.card.suit;
+
 	return (
-		<span className={styles.miniCard}>
+		<span className={styles.miniCard} style={{color: suitToColor[suit]}}>
 			{shortValue(props.card.value)}
-			<img alt={props.card.suit} className={styles.miniSuit} src={suitToFilename[props.card.suit]}></img>
+			<img alt={suit} className={styles.miniSuit} src={suitToFilename[suit]}></img>
 		</span>
 	)
 }
